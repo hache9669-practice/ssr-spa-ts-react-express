6413e9277b02554228d409c7cc430b65aa09af7e
hache9669 <hm.ck.rc@gmail.com>
2020-02-22 02:24:28 +0900

    ssr

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import ssr from './views/ssr';
 const app = express();
 app.listen(3000);
 
+app.use(express.static('assets'));
+
 app.get('/', (_, res) => {
   const response = ssr();
   res.send(response);
